100d86595e540b49ca43d21798d0e7caa8b3c952
Andrew Lee <candrewlee14@gmail.com>
2020-09-20 01:53:15 -0600

    implement serialization/deserialization for models and use those models in canvas connector

--- canvas_connector.py
+++ canvas_connector.py
@@ -3,7 +3,8 @@ import json
 import datetime
 from pprint import pprint
 from utilities import chunks
-
+from models import CanvasAssignment, CanvasCourse
+from typing import List
 # load config file
 with open('config.json') as config_file:
     data = json.load(config_file)
@@ -14,21 +15,22 @@ PER_PAGE = 50
 SESSION = requests.Session()
 SESSION.headers.update({'Authorization': f'Bearer {data["CANVAS_TOKEN"]}'})
 
-def get_course_contexts():
+
+def get_all_courses():
     """Get all user canvas course ids and return list of contexts"""
     course_params = {
             'enrollment_state':'active',
             'per_page': PER_PAGE,
         }
     courses_res = SESSION.get('https://' + data["CANVAS_URL"] + '/api/v1/courses', params=course_params)
-
+    courses = [CanvasCourse.from_dict(course) for course in courses_res.json()]
     #Canvas expects contexts for courses to be course_:id
-    course_contexts = ['course_' + str(course['id']) for course in courses_res.json() if 'name' in course];
-    return course_contexts
+    return courses
 
 def get_all_assignments():
     """Get all canvas assignments in all courses for the next month"""
-    course_contexts = get_course_contexts()
+    courses = get_all_courses()
+    course_contexts = ['course_' + str(course.id) for course in courses]
     # set date range for getting assignments
     date_now = datetime.datetime.utcnow()
     month_delta = datetime.timedelta(days=365/12)
@@ -37,7 +39,7 @@ def get_all_assignments():
     # Canvas API only takes at max 10 contexts
     # break up contexts into chunks 
     course_chunks = list(chunks(course_contexts, CHUNK_SIZE))
-    all_assignments = list()
+    all_assignments : List[CanvasAssignment] = list()  
     # for each chunk, request the assignment calendar events
     for course_context_chunk in course_chunks:
         cal_params = {
@@ -49,8 +51,6 @@ def get_all_assignments():
             }
         assignment_events_res = SESSION.get('https://' + data["CANVAS_URL"] + '/api/v1/calendar_events', params=cal_params)
         #pull the assignment out of the assignment event and put them into the list
-        all_assignments.extend([event['assignment'] for event in assignment_events_res.json()])
+        all_assignments.extend([CanvasAssignment.from_dict(event['assignment']) for event in assignment_events_res.json()])
         
     return all_assignments
-
-#pprint(get_all_assignments())
--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import json
-import datetime
 
-class JsonObject():
+class JsonSerializableBase():
+    """JSON-Serializable base class"""
     def __init__(self):
         self.id = None
 
@@ -17,17 +17,64 @@ class JsonObject():
         return json.dumps(self, default=lambda o: o.get_fields_dict(includeId), 
             sort_keys=True, indent=4)
 
-class TodoTask(JsonObject):
-    def __init__(self, title:str, dueDateTime:str, reminderDateTime:str, id = None):
+class TodoTask(JsonSerializableBase):
+    """Microsoft To Do Task class that helps in JSON serialization/deserialization"""
+    def __init__(self, title: str, dueDateTime: str, reminderDateTime: str, id = None):
         super().__init__()
         self.title = title
         self.isReminderOn = True
         self.dueDateTime = dueDateTime
         self.reminderDateTime =  reminderDateTime
-            
-class CanvasAssignment(JsonObject):
-    def __init__(self, name:str, dueDateTime:str, html_url:str, id = None):
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        try:
+            return cls(
+                data['title'],
+                data['isReminderOn'],
+                data['dueDateTime'],
+                data['reminderDateTime'],
+                id = data['id']
+            ) 
+        except:
+            print("TodoTask dictionary came in unnexpected format")
+            quit()
+
+class CanvasCourse():
+    """Canvas Course class that helps in JSON deserialization"""
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        try:
+            return cls(
+                data['id'],
+                data['name']
+            ) 
+        except:
+            print("CanvasCourse dictionary came in unnexpected format")
+            quit()
+
+
+class CanvasAssignment():
+    """Canvas Assignment class that helps in JSON deserialization"""
+    def __init__(self, name: str, dueDateTime: str, html_url: str, id = None):
         super().__init__()
         self.name = name
         self.due_at = dueDateTime
         self.html_url = html_url
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        try:
+            return cls(
+                data['name'],
+                data['due_at'],
+                data['html_url'],
+                id = data['id']
+            ) 
+        except:
+            print("CanvasAssignment dictionary came in unnexpected format")
+            quit()
