f0bf70310a023811954f3cf7c11d49193220acbf
Andrew Lee <candrewlee14@gmail.com>
2020-09-20 04:38:58 -0600

    fix and improve serialization/deserialization to align with canvas/microsoft graph APIs

--- models.py
+++ models.py
@@ -17,28 +17,9 @@ class JsonSerializableBase():
         return json.dumps(self, default=lambda o: o.get_fields_dict(includeId), 
             sort_keys=True, indent=4)
 
-class TodoTask(JsonSerializableBase):
-    """Microsoft To Do Task class that helps in JSON serialization/deserialization"""
-    def __init__(self, title: str, dueDateTime: str, reminderDateTime: str, id = None):
-        super().__init__()
-        self.title = title
-        self.isReminderOn = True
-        self.dueDateTime = dueDateTime
-        self.reminderDateTime =  reminderDateTime
 
-    @classmethod
-    def from_dict(cls, data: dict):
-        try:
-            return cls(
-                data['title'],
-                data['isReminderOn'],
-                data['dueDateTime'],
-                data['reminderDateTime'],
-                id = data['id']
-            ) 
-        except:
-            print("TodoTask dictionary came in unnexpected format")
-            quit()
+
+        
 
 class CanvasCourse():
     """Canvas Course class that helps in JSON deserialization"""
@@ -60,12 +41,13 @@ class CanvasCourse():
 
 class CanvasAssignment():
     """Canvas Assignment class that helps in JSON deserialization"""
-    def __init__(self, name: str, dueDateTime: str, html_url: str, id = None):
+    def __init__(self, name: str, dueDateTime: str, html_url: str, course_id: int, id = None):
         super().__init__()
         self.name = name
         self.due_at = dueDateTime
         self.html_url = html_url
-
+        self.course_id = course_id
+        self.id = id
     @classmethod
     def from_dict(cls, data: dict):
         try:
@@ -73,8 +55,45 @@ class CanvasAssignment():
                 data['name'],
                 data['due_at'],
                 data['html_url'],
+                int(data['course_id']),
                 id = data['id']
             ) 
         except:
             print("CanvasAssignment dictionary came in unnexpected format")
             quit()
+
+class TodoTask(JsonSerializableBase):
+    """Microsoft To Do Task class that helps in JSON serialization/deserialization"""
+    def __init__(self, title: str, body: str, isReminderOn: bool, dueDateTime: str = None, reminderDateTime: str = None, id = None):
+        super().__init__()
+        self.title = title
+        self.body = body
+        self.isReminderOn = isReminderOn
+        self.dueDateTime = dueDateTime
+        self.reminderDateTime =  reminderDateTime
+        self.id = id
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        try:
+            return cls(
+                data['title'],
+                data['body']['content'],
+                data['isReminderOn'],
+                # assumes UTC timezone. This may need a fix in the future
+                dueDateTime = data.get('dueDateTime', {}).get('dateTime', None),
+                reminderDateTime = data.get('reminderDateTime', {}).get('dateTime', None),
+                id = data['id']
+            ) 
+        except:
+            print("TodoTask dictionary came in unnexpected format")
+            quit()
+
+    def to_json(self):
+        json_dict = dict()
+        json_dict['title'] = self.title
+        json_dict['isReminderOn'] = self.isReminderOn
+        json_dict['dueDateTime'] = {'dateTime': self.dueDateTime, 'timeZone' : 'UTC'}
+        json_dict['body'] = {'content' : self.body, 'contentType': 'text'}
+        json_dict['reminderDateTime'] = {'dateTime': self.reminderDateTime, 'timeZone' : 'UTC'}
+        return json_dict
